83379e7215630715425a3b660a0b25a175bf3b7e
Durre shewar Malik <durreshewarmalik@Durres-MacBook-Pro.local>
2021-02-02 19:15:38 +0100

    tryoing to add life and score

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let hero = document.getElementById("hero");
 let enemy = document.getElementById("enemy");
 let counter = 0;
 let bottom = 0;
+let score = 0;
+let life = 3;
 
-function goUp() { //Jannes kod, jumping function for the hero
+function goUp() { //Janne's code, jumping function for the hero
     counter = 0;
     let timer = setInterval(function() {
         counter++
@@ -19,7 +21,7 @@ function goUp() { //Jannes kod, jumping function for the hero
         hero.style.bottom = bottom + "px";
         }, 40);
         }
-        console.log("Hej!" + counter);
+        //console.log("Hej!" + counter);
         hero.style.bottom = bottom + "px";
     }, 40);
 }
@@ -29,18 +31,29 @@ document.addEventListener("keyup", function(e) { //only one key (ArrowUp) is nee
     switch (e.key) {
         case "ArrowUp":
             hero.classList.add("keyUp");
-            console.log("hi");
+            //console.log("hi");
             goUp();
         break                
         };
     });
 
-let life = setInterval(function(){ //we set of 10 sec, where we check the position of both blocks and if the collide we set game to be over
+let lose = setInterval(function(){ //we set of 10 sec, where we check the position of both blocks and if the collide we set game to be over
     let heroBottom = parseInt(window.getComputedStyle(hero).getPropertyValue("bottom")); //getComputedStyle - taking all css propeties for hero, with getPropertyValue we are specifically taking the value of "bottom".
     let enemyLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue("left")); //getComputedStyle - taking all css propeties for enemy, with getPropertyValue we are specifically taking the value of "left".
-
     if (enemyLeft < 160 && enemyLeft > 60 && heroBottom <  50) { //condition for checking the collision
+    //    life-- 
+    //     console.log(life);
+    //     if (life == 0) {             (code not working for counting life)
+    //     alert("You lose!");
+    
         enemy.style.animation= "none";
+        hero.style.animation= "none";
         alert("You lose!");
-    };
+    }  
+//    else { 
+//         score++                (another piece of code for attemps of counting score)
+//         console.log(score);
+//     };
 }, 10);
+
+console.log("life");

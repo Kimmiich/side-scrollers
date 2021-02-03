58cb40c9ab5a33d64a6b9f010c7e04fe564680a7
Antonia-91 <76499167+Antonia-91@users.noreply.github.com>
2021-02-03 10:45:53 +0100

    added life counter

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let hero = document.getElementById("hero");
 let enemy = document.getElementById("enemy");
+const lifeValue = document.querySelector(".lifeValue");
+
 let counter = 0;
 let bottom = 0;
 let score = 0;
@@ -7,63 +9,82 @@ let life = 3;
 
 let isJumping = false; // To only jump once.
 
-function goUp() { //Janne's code, jumping function for the hero
-    counter = 0;
-    let timer = setInterval(function() {
-        counter++
-        bottom += 10;
-            
-        if (counter == 13) {
-            clearInterval(timer)
-            counter = 10;
-            let timerDown = setInterval(function() {
-                counter--;
-                bottom -= 13;
-                if (counter == 0) {
-                    clearInterval(timerDown)
-                    isJumping = false;
-                }
-                hero.style.bottom = bottom + "px";
-                console.log(hero.style.bottom);
-            }, 40);
+function goUp() {
+  //Janne's code, jumping function for the hero
+  counter = 0;
+  let timer = setInterval(function () {
+    counter++;
+    bottom += 10;
+
+    if (counter == 13) {
+      clearInterval(timer);
+      counter = 10;
+      let timerDown = setInterval(function () {
+        counter--;
+        bottom -= 13;
+        if (counter == 0) {
+          clearInterval(timerDown);
+          isJumping = false;
         }
-        //console.log("Hej!" + counter);
         hero.style.bottom = bottom + "px";
         console.log(hero.style.bottom);
-    }, 40);
+      }, 40);
+    }
+    //console.log("Hej!" + counter);
+    hero.style.bottom = bottom + "px";
+    console.log(hero.style.bottom);
+  }, 40);
 }
 
-document.addEventListener("keyup", function(e) { //only one key (ArrowUp) is needed for this game
-    console.log(e.key, hero);
-    switch (e.key) {
-        case "ArrowUp":
-            if (!isJumping) {
-                isJumping = true;
-                hero.classList.add("keyUp");
-                //console.log("hi");
-                goUp();
-            }
-        break                
-        };
-    });
+document.addEventListener("keyup", function (e) {
+  //only one key (ArrowUp) is needed for this game
+  console.log(e.key, hero);
+  console.log(e);
+  switch (e.key) {
+    case "ArrowUp":
+      if (!isJumping) {
+        isJumping = true;
+        //hero.classList.add("heroStyle"); // class not existing
+        //console.log("hi");
+        goUp();
+      }
+      break;
+    case "Space":
+  }
+});
 
-let lose = setInterval(function(){ //we set of 10 sec, where we check the position of both blocks and if the collide we set game to be over
-    let heroBottom = parseInt(window.getComputedStyle(hero).getPropertyValue("bottom")); //getComputedStyle - taking all css propeties for hero, with getPropertyValue we are specifically taking the value of "bottom".
-    let enemyLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue("left")); //getComputedStyle - taking all css propeties for enemy, with getPropertyValue we are specifically taking the value of "left".
-    if (enemyLeft < 160 && enemyLeft > 60 && heroBottom <  50) { //condition for checking the collision
-    //    life-- 
-    //     console.log(life);
-    //     if (life == 0) {             (code not working for counting life)
-    //     alert("You lose!");
-    
-        enemy.style.animation= "none";
-        hero.style.animation= "none";
-        alert("You lose!");
-    }  
-//    else { 
-//         score++                (another piece of code for attemps of counting score)
-//         console.log(score);
-//     };
-}, 10);
+let lose = setInterval(function () {
+  //we set of 10 sec, where we check the position of both blocks and if the collide we set game to be over
+  let heroBottom = parseInt(
+    window.getComputedStyle(hero).getPropertyValue("bottom")
+  ); //getComputedStyle - taking all css propeties for hero, with getPropertyValue we are specifically taking the value of "bottom".
+  let enemyLeft = parseInt(
+    window.getComputedStyle(enemy).getPropertyValue("left")
+  ); //getComputedStyle - taking all css propeties for enemy, with getPropertyValue we are specifically taking the value of "left".
+  if (enemyLeft < 160 && enemyLeft > 60 && heroBottom < 50) {
+    //condition for checking the collision
+    life--;
+    console.log(life);
+    lifeValue.innerHTML = life;
+    // enemy.style.animation = "block";
+    enemy.style.animation = "none";
+    if (life == 0) {
+      //(code not working for counting life)
+
+      //enemy.style.animation = "none";
+      //hero.style.animation = "none"; not needed?
+      alert("You lose!");
+    }
+
+    //    else {
+    //         score++                (another piece of code for attemps of counting score)
+    //         console.log(score);
+    //     };
+  }
+}, 40);
 
 console.log("life");
+
+// knapp som sätter igång animationen
+
+//startButtom
